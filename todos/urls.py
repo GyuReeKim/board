d8dd25f6b01b0c038578b5f55c51f8a6c4730816
GyuReeKim <starkim96@naver.com>
2019-10-04 14:08:53 +0900

    new, create 합체

--- todos/urls.py
+++ todos/urls.py
@@ -5,10 +5,13 @@ from . import views # '.' 대신 'todos'를 써도 된다. '.' : 같은 폴더
 app_name = 'todos'
 
 urlpatterns = [
+    # Read
     path('', views.index, name="index"),
-
+    # Create
     path('new/', views.new, name="new"), # new/라는 경로를 new라는 변수에 저장해서 사용한다.
     path('create/', views.create, name="create"),
-
+    # new와 create를 합친다.
+    path('add/', views.add, name="add"),
+    # Delete
     path('<int:id>/delete/', views.delete, name="delete")
 ]
