a2f4c82755bf3055d5a87aa3c95e7f813f29a31e
GyuReeKim <starkim96@naver.com>
2019-10-04 11:13:21 +0900

    create 완성

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views # . 대신 todos를 써도 된다. '.' : 같은 폴더
+from . import views # '.' 대신 'todos'를 써도 된다. '.' : 같은 폴더
 
 urlpatterns = [
     path('', views.index),
+
+    path('new/', views.new),
+    path('create/', views.create),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo # '.models' 대신 'todos.models'를 사용해도 된다.
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'index.html', context)
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    author = request.POST.get('author')
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    due_date = request.POST.get('due-date')
+
+    # todo = Todo()
+    # todo.author = author
+    # todo.title = title
+    # todo.content = content
+    # todo.due_date = due_date
+    # todo.save()
+
+    # create는 안에 인스턴스를 만들고 생성까지 해준다.
+    todo = Todo.objects.create(
+        author=author,
+        title=title,
+        content=content,
+        due_date=due_date,
+    )
+    return redirect('/todos/')
